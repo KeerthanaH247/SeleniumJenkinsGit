ed4d7908dc85d237031a314867f315804493e770
Keerthana H <keerthanadas247@gmail.com>
2025-05-24 11:13:26 +0530

    Jenkins demo

---
+++ src/test/java/OpenBrowser/OpenOrangeHRM.java
@@ -0,0 +1,22 @@
+package OpenBrowser;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class OpenOrangeHRM {
+
+	@Test
+	public void open()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.quit();
+	}
+}
