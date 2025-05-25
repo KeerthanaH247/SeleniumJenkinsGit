782ba4d58a706906f90dfaad64301d22f5470591
Keerthana H <keerthanadas247@gmail.com>
2025-05-25 11:53:31 +0530

    passing parameter

--- src/test/java/OpenBrowser/OpenOrangeHRM.java
+++ src/test/java/OpenBrowser/OpenOrangeHRM.java
@@ -1,5 +1,6 @@
 package OpenBrowser;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,10 +10,11 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class OpenOrangeHRM {
-
+	@Parameters("Browser")
 	@Test
-	public void open()
+	public void open(String browserName)
 	{
+		System.out.println("Parameter name is "+ browserName);
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
